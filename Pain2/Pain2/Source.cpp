7021fe2e0087fcd0f985cbf113bed9a50ddc1c82
NovaBSU <115707004+NovaBSU@users.noreply.github.com>
2022-10-18 15:48:15 +0100

    ggggg

---
+++ Pain2/Pain2/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int x;
+	cout << "What is your number? "; // talk to user
+	cin >> x; // take input
+	if (x == 25) {
+		cout << "Your Number is 25";
+	}
+	else if(x < 25) {
+		cout << "Your Number is less than 25";
+	}
+	else {
+		cout << "Your Number is greater than 25";
+	}
+
+	return 0;
+	
+
+}
---
+++ pain/Project1/pain.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Coding is Pain" << endl;
+
+    int firstNumber = 1;
+    bool iCanCode = true;
+    string hopefulGrade = "a";
+    float myDecimal = 1.0;
+    string minimalSentance = "y";
+    int keyMash = 13213123;
+    float mysteryDataType = 5.6f; //Use Google to work this out
+
+    float numberOne = 50;
+    float numberTwo = 7;
+    cout << numberOne / numberTwo;
+
+    return 0;
+}
